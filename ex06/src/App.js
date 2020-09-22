94ac0476d55c5a463f144c441ecab3ab5eaf8ddb
Felizardo <devfelizardo@gmail.com>
2020-09-22 02:47:25 -0300

    coding for ex06 done

--- ex06/src/App.js
+++ ex06/src/App.js
@@ -1,6 +1,25 @@
 import React from "react";
 
 const App = () => {
-  return null;
+  const [counter, setCounter] = React.useState(0);
+  const [notification, setNotification] = React.useState(null);
+  const timeoutRef = React.useRef();
+
+  function handleClick() {
+    setNotification("Item added to the kart");
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      setNotification(null);
+    }, 1000);
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <p>{notification}</p>
+      <button onClick={handleClick}>{counter}</button>
+    </div>
+  );
 };
+
 export default App;
