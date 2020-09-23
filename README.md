2f79a2785a888fc991b5d06a20e8ccb66149a6be
Felizardo <devfelizardo@gmail.com>
2020-09-23 04:14:03 -0300

    coding for exercise 08 completed

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ This is a repository to save a series of exercises done for practice and review 
 - ex05: Similar to ex04 but adding useEffect and LocalStorage.
 - ex06: Add item pop up and pop out after 2s using useRef.
 - ex07: List all covid cases per country using useContext to pass information to all components.
+- ex08: Fetch and Local Storage as a Custom Hooks for listing all countries and saving data locally.
--- ex08/src/App.js
+++ ex08/src/App.js
@@ -1,6 +1,42 @@
 import React from "react";
+import useLocalStorage from "./useLocalStorage";
+import useFetch from "./useFetch";
 
 const App = () => {
-  return null;
+  const [country, setCountry] = useLocalStorage("country", "");
+  const { request, data, loading, error } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const { response, json } = await request(
+        "https://coronavirus-19-api.herokuapp.com/countries"
+      );
+      console.log(response, json);
+    }
+    fetchData();
+  }, [request]);
+
+  function handleClick({ target }) {
+    setCountry(target.innerText);
+  }
+
+  if (error) return <p>{error}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (data)
+    return (
+      <div>
+        <p>Country Saved Locally: {country}</p>
+        <button onClick={handleClick}>USA</button>
+        <button onClick={handleClick}>UK</button>
+
+        {data.map((elem) => (
+          <div key={elem.country}>
+            <div>{elem.country}</div>
+          </div>
+        ))}
+      </div>
+    );
+  else return null;
 };
+
 export default App;
