13f4a923e1bdf8daeae6c04814c25a77893b05c6
Felizardo <devfelizardo@gmail.com>
2020-10-10 03:17:08 -0300

    done exercise with simple one field form and print

--- ex09/src/App.js
+++ ex09/src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
 
 const App = () => {
-  return null;
+  const [nameInput, setNameInput] = React.useState('');
+
+  return (
+  <form>
+    <input 
+      type="text" 
+      value={nameInput} 
+      onChange={(event) => setNameInput(event.target.value)}
+    />
+    <p> Text: {nameInput} </p> 
+  </form>
+  );
 };
+
 export default App;
