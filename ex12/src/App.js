5a2fc28095da76301ba8969e004c1691c95bc197
Felizardo <devfelizardo@gmail.com>
2020-10-24 23:17:46 -0300

    coding select option for exercise 12

--- ex12/src/App.js
+++ ex12/src/App.js
@@ -2,15 +2,30 @@ import React from "react";
 
 const App = () => {
   const [description, setDescription] = React.useState("");
+  const [gender, setGender] = React.useState("");
 
   return (
     <form>
       <textarea 
         value={description}
-        onChange={({target}) => setDescription(target.value)}
+        //Next code line is replacing: 
+        //onChange={(event) => setDescription(event.target.value)}
+        onChange={({target}) => setDescription(target.value)} 
         rows="6"
       />
       {description}
+      
+      <select 
+        value={gender} 
+        onChange={(event) => setGender(event.target.value)}
+        id="gender"
+      >
+        {/* Default selected option, another way is to start state with one valid value*/}
+        <option disabled value="">Choose</option> 
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+      {gender}
     </form>
   );
 };
